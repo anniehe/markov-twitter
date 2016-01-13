48dc620334fed466a597ef9d7ce521b4c4dd67b4
Hackbright Student <info@hackbrightacademy.com>
2016-01-13 13:04:04 -0800

    working on tweet again

--- markov.py
+++ markov.py
@@ -79,7 +79,29 @@ def tweet(chains):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
-    make_text(chains)
+
+    api = twitter.Api(
+        consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+    # print api.VerifyCredentials()
+
+    # Send a tweet with our Markov text
+    status = api.PostUpdate(make_text(chains))
+    print status.text
+
+    
+def tweet_again():    
+
+    while True:
+        tweet_again_answer = raw_input("Press any key to tweet again [q to quit] > ")
+        if tweet_again_answer == 'q':
+            break
+        else:
+            tweet(chains)
+
 
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
@@ -92,4 +114,4 @@ text = open_and_read_file(filenames)
 chains = make_chains(text)
 
 # Your task is to write a new function tweet, that will take chains as input
-# tweet(chains)
+tweet(chains)
